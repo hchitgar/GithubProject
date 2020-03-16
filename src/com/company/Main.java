a28cfd3aea0ec8725157836d2728646da2fdd67e
FFI <ic.chitgar@gmail.com>
2020-03-16 09:57:56 +0330

    commit by my_branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
 
         System.out.println("Hello github from intelliJ IDEA");
         System.out.println("Hello github from intelliJ IDEA from my_branch");
+        System.out.println("Hello github from intelliJ IDEA 22222 from my_branch");
     }
 }
